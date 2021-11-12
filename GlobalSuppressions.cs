b143b885fa8fb77b8c9f5fd4c368ce310bdbd92d
alchemistmatt <matthew.monroe@pnnl.gov>
2021-11-12 10:32:31 -0800

    Suppress warning

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Usage", "RCS1146:Use conditional access.", Justification = "Leave as-is", Scope = "member", Target = "~M:SQLServer_Stored_Procedure_Converter.StoredProcedureConverter.AppendLine(System.Collections.Generic.ICollection{System.String},System.String)")]
