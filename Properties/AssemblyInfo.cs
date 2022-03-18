e6d329747e53ec3c5f9d1a2ce1012a36b5533cf0
alchemistmatt <matthew.monroe@pnnl.gov>
2022-03-18 16:30:25 -0700

    Remove unused using statement

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -30,4 +29,3 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("1.0.*")]
-
